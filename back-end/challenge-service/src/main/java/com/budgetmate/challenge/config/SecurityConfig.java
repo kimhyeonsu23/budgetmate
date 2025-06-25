ce03052665ab6fd478c99fb5eea3630075333625
김현수 <gimhyeonsu@gimhyeonsus-MacBook-Air.local>
2025-06-25 17:42:29 +0900

    config파일 수정 추가

--- back-end/challenge-service/src/main/java/com/budgetmate/challenge/config/SecurityConfig.java
+++ back-end/challenge-service/src/main/java/com/budgetmate/challenge/config/SecurityConfig.java
@@ -2,22 +2,26 @@ package com.budgetmate.challenge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/**")
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().permitAll()  // 모든 요청 허용 (인증 없이)
-                )
-                .csrf((csrf) -> csrf.disable());  // CSRF 비활성화
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) { // ServerHttpSecurity : spring Security에서 제공하는 webFlux용 안 설정 빌더 클래스
 
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(
+                        exchange
+                                -> exchange
+                                .anyExchange().permitAll()
+                )
+                .build();
+
     }
+
 }
