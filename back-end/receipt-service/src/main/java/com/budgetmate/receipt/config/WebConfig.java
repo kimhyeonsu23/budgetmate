3ead0302219cc3f528b1ff24fce44cfbe3ba07bc
김현수 <gimhyeonsu@gimhyeonsus-MacBook-Air.local>
2025-06-25 20:00:48 +0900

    path 원상복구

--- back-end/receipt-service/src/main/java/com/budgetmate/receipt/config/WebConfig.java
+++ back-end/receipt-service/src/main/java/com/budgetmate/receipt/config/WebConfig.java
@@ -8,29 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.file.Paths;
 
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Value("${file.upload-dir}")
-//    private String uploadPath;
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        String resolvedPath = "file:" + (uploadPath.endsWith("/") ? uploadPath : uploadPath + "/");
-//        registry
-//                .addResourceHandler("/receipt/image/**")
-//                .addResourceLocations(resolvedPath);
-//    }
-//}
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final String uploadPath = "file:/upload/image/"; // 절대경로 + file: 접두사
+    @Value("${file.upload-dir}")
+    private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String resolvedPath = "file:" + (uploadPath.endsWith("/") ? uploadPath : uploadPath + "/");
         registry
                 .addResourceHandler("/receipt/image/**")
-                .addResourceLocations(uploadPath);
+                .addResourceLocations(resolvedPath);
     }
 }
